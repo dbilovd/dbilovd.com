4bed2212473f53c11a1e0cdf1c56146e0a2f665d
David Lartey <dbilovd@gmail.com>
2019-09-23 01:43:32 +0000

    switched places of updates and releases on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,33 @@
 		<div class="bg-white py-20 px-10">
 			<div class="max-w-4xl mx-auto">
 				<div class="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-10">
+					<h3 class="uppercase font-semibold text-xl">Latest Updates</h3>
+					<router-link to="/updates" 
+						class="block uppercase tracking-wide text-gray-600 border-b-4 border-gray-600 mt-4 md:mt-0">
+						See More ...
+					</router-link>
+				</div>
+				<h3 v-if="posts.length == 0" class="capitalize text-3xl mb-20">No Update Right Now</h3>
+				<div class="flex flex-col md:flex-row justify-start mb-20 md:-ml-6" v-else>
+					<div class="w-full md:w-1/3 mt-6 md:mt-0 md:ml-6" v-for="(post, postIndex) in posts" :key="postIndex">
+						<div class="bg-white shadow-lg border rounded w-full overflow-hidden">
+							<img :src="post.image" :alt="post.title" class="w-full h-48 object-cover object-top">
+							<div class="p-4">
+								<h3 class="font-semibold capitalize text-lg">
+									<a :href="`/updates/${post.slug}`">{{ post.title }}</a>
+								</h3>
+								<p class="text-gray-600 h-20 overflow-hidden">{{ post.intro }}</p>
+								<a :href="`/updates/${post.slug}`" class="inline-block mt-2 capitalize tracking-wide text-xs font-semibold border-b border-gray-800">Continue...</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="bg-gray-100 py-20 px-10">
+			<div class="max-w-4xl mx-auto">
+				<div class="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-10">
 					<h3 class="uppercase font-semibold text-xl">Latest Releases</h3>
 					<router-link to="/releases" 
 						class="block uppercase tracking-wide text-gray-600 border-b-4 border-gray-600 mt-4 md:mt-0">
@@ -55,33 +82,6 @@
 							</svg>
 							<span>Free Downloads</span>
 						</router-link>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="bg-gray-100 py-20 px-10">
-			<div class="max-w-4xl mx-auto">
-				<div class="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-10">
-					<h3 class="uppercase font-semibold text-xl">Latest Updates</h3>
-					<router-link to="/updates" 
-						class="block uppercase tracking-wide text-gray-600 border-b-4 border-gray-600 mt-4 md:mt-0">
-						See More ...
-					</router-link>
-				</div>
-				<h3 v-if="posts.length == 0" class="capitalize text-3xl mb-20">No Update Right Now</h3>
-				<div class="flex flex-col md:flex-row justify-start mb-20 md:-ml-6" v-else>
-					<div class="w-full md:w-1/3 mt-6 md:mt-0 md:ml-6" v-for="(post, postIndex) in posts" :key="postIndex">
-						<div class="bg-white shadow-lg border rounded w-full overflow-hidden">
-							<img :src="post.image" :alt="post.title" class="w-full h-48 object-cover object-top">
-							<div class="p-4">
-								<h3 class="font-semibold capitalize text-lg">
-									<a :href="`/updates/${post.slug}`">{{ post.title }}</a>
-								</h3>
-								<p class="text-gray-600 h-20 overflow-hidden">{{ post.intro }}</p>
-								<a :href="`/updates/${post.slug}`" class="inline-block mt-2 capitalize tracking-wide text-xs font-semibold border-b border-gray-800">Continue...</a>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
