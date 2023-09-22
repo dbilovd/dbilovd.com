82595434868f87524e11a6adeb67c93c2fb1137e
David Lartey <dbilovd@gmail.com>
2023-09-22 12:41:00 +0000

    Added banner on top of site

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,72 @@
 <template>
   <div id="app">
+    <div class="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+      <p class="text-sm leading-6 text-white">
+        <a href="https://fanlink.to/mPnA" target="__blank">
+          New Single: A Dreamer's Song (Working for Good) & Photobook 
+          <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
+          <span class="whitespace-nowrap font-semibold">
+            Stream and Download the Visual Bundle&nbsp;
+            <span aria-hidden="true">&rarr;</span>
+          </span>
+        </a>
+      </p>
+      <div class="flex flex-1 justify-end">
+      </div>
+    </div>
+
     <div class="">
-      <nav class="flex flex-col md:flex-row md:justify-center items-center bg-white px-8 py-4">
+      <nav
+        class="flex flex-col md:flex-row md:justify-center items-center bg-white px-8 py-4"
+      >
         <div class="hidden md:block md:mr-24">
           <router-link to="/releases" class="font-semibold">Music</router-link>
-          <a href="https://genius.com/artists/Dbilovd" target="__blank" class="font-semibold ml-6">Lyrics</a>
+          <a
+            href="https://genius.com/artists/Dbilovd"
+            target="__blank"
+            class="font-semibold ml-6"
+            >Lyrics</a
+          >
         </div>
         <h1 class="font-bold text-2xl">
           <router-link to="/">Dbilovd</router-link>
         </h1>
         <div class="hidden md:block md:ml-24">
           <router-link to="/about" class="font-semibold">About</router-link>
-          <router-link to="/freely-given" class="font-semibold ml-6">#FreelyGiven</router-link>
+          <router-link to="/freely-given" class="font-semibold ml-6"
+            >#FreelyGiven</router-link
+          >
         </div>
 
         <div class="block md:hidden mt-2">
           <router-link to="/releases" class="">Music</router-link>
-          <a href="https://genius.com/artists/Dbilovd" target="__blank" class="ml-4">Lyrics</a>
+          <a
+            href="https://genius.com/artists/Dbilovd"
+            target="__blank"
+            class="ml-4"
+            >Lyrics</a
+          >
           <router-link to="/about" class="ml-4">About</router-link>
           <router-link to="/support" class="ml-4">Support</router-link>
         </div>
       </nav>
     </div>
 
-    <slot/>
+    <slot />
 
     <FooterComponent></FooterComponent>
   </div>
 </template>
 
 <script type="text/javascript">
-  'use strict';
-  import FooterComponent from '~/components/Footer';
+"use strict";
+import FooterComponent from "~/components/Footer";
 
-  const DefaultLayout = {
-    components: {
-      FooterComponent,
-    }
-  };
+const DefaultLayout = {
+  components: {
+    FooterComponent,
+  },
+};
 
-  export default DefaultLayout;
+export default DefaultLayout;
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -22,59 +22,6 @@
       </div>
     </div>
 
-    <div class="bg-white py-20 px-10">
-      <div class="max-w-4xl mx-auto">
-        <div
-          class="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-10"
-        >
-          <h3 class="uppercase font-semibold text-xl">Latest Updates</h3>
-          <nuxt-link
-            to="/updates"
-            class="block uppercase tracking-wide text-gray-600 border-b-4 border-gray-600 mt-4 md:mt-0"
-          >
-            See More ...
-          </nuxt-link>
-        </div>
-        <h3 v-if="posts.length == 0" class="capitalize text-3xl mb-20">
-          No Update Right Now
-        </h3>
-        <div
-          class="flex flex-col md:flex-row justify-start mb-20 md:-ml-6"
-          v-else
-        >
-          <div
-            class="w-full md:w-1/3 mt-6 md:mt-0 md:ml-6"
-            v-for="(post, postIndex) in posts"
-            :key="postIndex"
-          >
-            <div
-              class="bg-white shadow-lg border rounded w-full overflow-hidden"
-            >
-              <img
-                :src="post.image"
-                :alt="post.title"
-                class="w-full h-48 object-cover object-top"
-              />
-              <div class="p-4">
-                <h3 class="font-semibold capitalize text-lg">
-                  <a :href="`/updates/${post.slug}`">{{ post.title }}</a>
-                  <a :href="`/updates/${post.slug}`">{{ post.title }}</a>
-                </h3>
-                <p class="text-gray-600 h-20 overflow-hidden">
-                  {{ post.intro }}
-                </p>
-                <a
-                  :href="`/updates/${post.slug}`"
-                  class="inline-block mt-2 capitalize tracking-wide text-xs font-semibold border-b border-gray-800"
-                  >Continue...</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <div class="bg-gray-100 py-20 px-10">
       <div class="max-w-4xl mx-auto">
         <div
@@ -139,6 +86,59 @@
               </svg>
               <span>Free Downloads</span>
             </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white py-20 px-10">
+      <div class="max-w-4xl mx-auto">
+        <div
+          class="flex flex-col md:flex-row md:justify-between items-start md:items-center mb-10"
+        >
+          <h3 class="uppercase font-semibold text-xl">Latest Updates</h3>
+          <nuxt-link
+            to="/updates"
+            class="block uppercase tracking-wide text-gray-600 border-b-4 border-gray-600 mt-4 md:mt-0"
+          >
+            See More ...
+          </nuxt-link>
+        </div>
+        <h3 v-if="posts.length == 0" class="capitalize text-3xl mb-20">
+          No Update Right Now
+        </h3>
+        <div
+          class="flex flex-col md:flex-row justify-start mb-20 md:-ml-6"
+          v-else
+        >
+          <div
+            class="w-full md:w-1/3 mt-6 md:mt-0 md:ml-6"
+            v-for="(post, postIndex) in posts"
+            :key="postIndex"
+          >
+            <div
+              class="bg-white shadow-lg border rounded w-full overflow-hidden"
+            >
+              <img
+                :src="post.image"
+                :alt="post.title"
+                class="w-full h-48 object-cover object-top"
+              />
+              <div class="p-4">
+                <h3 class="font-semibold capitalize text-lg">
+                  <a :href="`/updates/${post.slug}`">{{ post.title }}</a>
+                  <a :href="`/updates/${post.slug}`">{{ post.title }}</a>
+                </h3>
+                <p class="text-gray-600 h-20 overflow-hidden">
+                  {{ post.intro }}
+                </p>
+                <a
+                  :href="`/updates/${post.slug}`"
+                  class="inline-block mt-2 capitalize tracking-wide text-xs font-semibold border-b border-gray-800"
+                  >Continue...</a
+                >
+              </div>
+            </div>
           </div>
         </div>
       </div>
