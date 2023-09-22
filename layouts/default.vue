dad0ca759cac4f98ce5683e910d362fe40a3256b
David Lartey <dbilovd@gmail.com>
2023-09-22 12:44:41 +0000

    Changed banner link to email signup

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <p class="text-sm leading-6 text-white">
-        <a href="https://fanlink.to/mPnA" target="__blank">
+        <a href="https://dbilovdmusic.ck.page/f1b8d0883d" target="__blank">
           New Single: A Dreamer's Song (Working for Good) & Photobook 
           <svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
           <span class="whitespace-nowrap font-semibold">
